5b8f506f5d05e410e5c105d2a0294bd82623ac6d
Thomas Boehringer <git@tboehringer.de>
2021-04-04 21:36:14 +0200

    add sticky footer

--- main.js
+++ main.js
@@ -2,7 +2,15 @@ const app = Vue.createApp({
     data() {
         return {
             name: 'KICKET',
-            description: 'KICKET helps to generate random teams for table soccer'
+            description: 'KICKET helps to generate random teams for table soccer',
+            author: 'Thomas Boehringer',
+            email: 'git@tboehringer.de',
+            website: 'tboehringer.de'
+        }
+    },
+    computed: {
+        getCopyright() {
+            return '&copy; 2021 Copyright: <a href="' + this.website + '"> ' + this.website + ' </a>'
         }
     }
 })
