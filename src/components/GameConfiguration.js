a074482032064b3825800e88745765e04bcb29f0
Thomas Boehringer <git@tboehringer.de>
2021-04-05 13:12:23 +0200

    add more players, generate multiple teams, get random player names

--- src/components/GameConfiguration.js
+++ src/components/GameConfiguration.js
@@ -9,14 +9,15 @@ app.component('game-configuration', {
     return {
       title: 'Game Configuration',
       desc: 'Add names of the players',
-      players: ['Proton', 'Atom', 'Helium', 'Calcium']
+      players: ['Proton', 'Atom', 'Helium', 'Calcium'],
+      defaultNames: []
     }
   },
   template:
     /*html*/
     `<div v-if="!started">
-      <h2>{{ title }}:</h2>
-      <p>{{ desc }}</p>
+      <h2>{{ title }}</h2>
+      <p>{{ desc }} <span><button v-on:click="newPlayer" class="btn btn-sm btn-success">+</button></span></p>
       <div id="players">
         <div
           v-for="(player, index) in players"
@@ -44,11 +45,21 @@ app.component('game-configuration', {
       </div>
     </div>`,
   methods: {
+    newPlayer() {
+      var name = this.defaultNames[Math.floor(Math.random() * (108 - 0) + 0)]
+      this.players.push(name)
+    },
     updatePlayer(index, name) {
       this.players[index] = name
     },
     startGame() {
       this.$emit('start-game', this.players)
-    },
+    }
+  },
+  mounted() {
+    fetch('./assets/data/defaultPlayerNames.json')
+      .then(res => res.json())
+      .then(data => this.defaultNames = data)
+      .catch(err => console.log(err.message))
   }
 })
--- src/main.js
+++ src/main.js
@@ -8,12 +8,10 @@ const app = Vue.createApp({
             website: 'tboehringer.de',
             players: [],
             started: false,
+            defaultTeamNames: []
         }
     },
     methods: {
-        addPlayer(name) {
-            this.players.push(name)
-        },
         startGame(players) {
             this.players = players
             this.started = true
@@ -24,14 +22,15 @@ const app = Vue.createApp({
         },
         generateTeams() {
             this.players.sort(function (a, b) { return 0.5 - Math.random() })
-            var teamBravo = this.players.slice()
-            var halfLength = Math.ceil(teamBravo.length / 2)
-            var teamAlpha = teamBravo.splice(0, halfLength)
-
-            return [
-                { 'name': 'alpha', 'members': teamAlpha },
-                { 'name': 'bravo', 'members': teamBravo }
-            ]
+            var tmpPlayers = this.players.slice()
+            var teams = [];
+            const sumOfTeams = Math.ceil(this.players.length / 2)
+            for (var i=0; i < sumOfTeams; i++) {
+                console.log(this.defaultTeamNames[i])
+                teams.push({ 'name': this.defaultTeamNames[i], 'members': tmpPlayers.splice(0,2) })
+            }
+            console.log(teams)
+            return teams;
         }
     },
     computed: {
@@ -41,5 +40,11 @@ const app = Vue.createApp({
         getPlayers() {
             return this.players;
         }
-    }
+    },
+    mounted() {
+        fetch('./assets/data/defaultTeamNames.json')
+          .then(res => res.json())
+          .then(data => this.defaultTeamNames = data)
+          .catch(err => console.log(err.message))
+      }
 })
