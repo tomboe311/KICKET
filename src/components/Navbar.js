4d8244f457ae6c605abf826461bc7549e2f37271
Thomas Boehringer <git@tboehringer.de>
2021-04-05 14:32:57 +0200

    add logos, change colors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@ app.component('knavbar', {
     template:
     /*html*/
     `
-    <nav class="navbar navbar-dark bg-dark py-0">
-        <a class="navbar-brand py-0" href="index.html">KICKET</a>    
+    <nav class="navbar navbar-dark py-0">
+        <a class="navbar-brand py-0" href="index.html"><img src="./assets/img/kicket-logo-nav.png" height="30" alt="KICKET"></a>    
     </nav>`
 })
